940a9f9caf59a17579cfcc6f4a97057e88eb7b92
Alif Saputra <f0u5@sunlife.com>
2025-07-09 16:39:24 +0700

    test create users

---
+++ components/users/user.component.js
@@ -0,0 +1,9 @@
+angular.module('userList')
+.component('userList', {
+    templateUrl: 'components/users/user.template.html',
+    controller:['$http',
+        function UserListController($http) {
+            
+        }
+    ]
+})
