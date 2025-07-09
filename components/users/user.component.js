8cd66d58ac09205a237f17bf8e08887eb40f1994
Alif Saputra <f0u5@sunlife.com>
2025-07-09 16:48:05 +0700

    update

--- components/users/user.component.js
+++ components/users/user.component.js
@@ -3,7 +3,16 @@ angular.module('userList')
     templateUrl: 'components/users/user.template.html',
     controller:['$http',
         function UserListController($http) {
-            
+            var ctrl = this;
+            ctrl.nama = '';
+            ctrl.users = ['Alif', 'Budi', 'Caca'];
+
+            ctrl.onAddUser = () => {
+                if(ctrl.nama.trim() !== ''){
+                    ctrl.users.push(ctrl.nama);
+                    ctrl.nama = '';
+                }
+            }
         }
     ]
 })
