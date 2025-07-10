70b6206817222eb7336db67a1b1fb5a9e521cb4f
Alif Saputra <f0u5@sunlife.com>
2025-07-10 09:16:45 +0700

    more templating

--- app.config.js
+++ app.config.js
@@ -6,6 +6,6 @@ angular.module('phonecatApp')
                     template: '<phone-list></phone-list>'
                 }).when('/phones/:phoneId', {
                 template: '<phone-detail></phone-detail>'
-            }).otherwise('/phones');
+            })
         }
     ])
--- components/phone-detail/phone-detail.component.js
+++ components/phone-detail/phone-detail.component.js
@@ -1,9 +1,13 @@
 angular.module('phoneDetail')
     .component('phoneDetail', {
-        template: 'TBD: Detail view {{$ctrl.phoneId}}',
-        controller: ['$routeParams', 
-            function phoneDetailController($routeParams){
+        templateUrl: 'components/phone-detail/phone-detail.template.html',
+        controller: ['$http', '$routeParams',
+            function phoneDetailController($http, $routeParams) {
                 this.phoneId = $routeParams.phoneId;
+
+                $http.get('assets/mocks/nexus-s.json').then((response) => {
+                    self.phone = response.data;
+                })
             }
         ]
     })
